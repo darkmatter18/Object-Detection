c10a7577d4d477dd5900ef8dee1560065f03082e
Arkadip Bhatttacharya <in2arkadipb13@gmail.com>
2024-12-26 13:49:24 +0530

    feat: Implement video processing and object detection methods in BaseDetection and Detectron classes

--- app/engine/detection/base.py
+++ app/engine/detection/base.py
@@ -1,3 +1,11 @@
+import logging
+from typing import Generator
+
+import cv2
+
+
+logger = logging.getLogger(__name__)
+
 class BaseDetection:
     def save(self, video: bytes, content_type: str) -> str:
         import tempfile
@@ -10,3 +18,36 @@ class BaseDetection:
         with tempfile.NamedTemporaryFile(delete=False, suffix=ext) as f:
             f.write(video)
             return f.name
+
+    def read_video(self, file_name: str) -> Generator[cv2.typing.MatLike, None, None]:
+        video = cv2.VideoCapture(file_name)
+
+        while video.isOpened():
+
+            has_frame, frame = video.read()
+
+            if has_frame:
+                    yield frame
+            else:
+                logger.warning("Error reading frame")
+                break
+        else:
+            video.release()
+            logger.error("Failed to read video feed")
+
+
+    def detect(self, file_name: str):
+        model_resp = []
+        for i, frame in enumerate(self.read_video(file_name)):
+            res = self.detect_frame_objects(frame)
+            print(f"Frame {i}: {res}")
+            model_resp.extend(res)
+        
+        ret_classess = self.classes_to_labels(list(set(model_resp)))
+        return ret_classess
+
+    def detect_frame_objects(self, frame):
+        raise NotImplementedError
+
+    def classes_to_labels(self, classes: list[int]) -> list[str]:
+        raise NotImplementedError
--- app/engine/detection/detectron.py
+++ app/engine/detection/detectron.py
@@ -1,4 +1,30 @@
+import torch
+
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.data import MetadataCatalog
+
 from .base import BaseDetection
 
+
 class Detectron(BaseDetection):
-    pass
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file('COCO-Detection/faster_rcnn_R_101_DC5_3x.yaml'))
+    cfg.MODEL.DEVICE = 'cpu'
+    predictor = DefaultPredictor(cfg)
+
+    metadata =  MetadataCatalog.get(
+        cfg.DATASETS.TEST[0] if len(cfg.DATASETS.TEST) else "__nonexist__"
+    )
+    class_catalog = metadata.thing_classes
+
+    def detect_frame_objects(self, frame) -> list[int]:
+        with torch.no_grad():
+            outputs = self.predictor(frame)
+            pred_classes = outputs['instances'].pred_classes.cpu().numpy().tolist()
+
+            return pred_classes
+
+    def classes_to_labels(self, classes: list[int]) -> list[str]:
+        return [self.class_catalog[i] for i in classes]
